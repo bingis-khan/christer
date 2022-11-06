9adeda92e97b83c144192b05ae271a3e2fcffb45
Bartosz Matysiak <qucker135@gmail.com>
2022-11-06 17:31:15 +0100

    Create swipe based on Swipe Cards

---
+++ christer/lib/model/content.dart
@@ -0,0 +1,10 @@
+import 'package:flutter/animation.dart';
+
+class Content{
+  String name;
+  String image_path;
+  String age;
+  List<String> interests;
+
+  Content({required this.name, required this.image_path, required this.age, required this.interests});
+}
--- christer/lib/pages/explore_page.dart
+++ christer/lib/pages/explore_page.dart
@@ -1,11 +1,11 @@
 import 'package:flutter/material.dart';
-//import 'package:flutter_svg/svg.dart';
-
 import 'package:christer/data/explore_json.dart';
 import 'package:christer/data/icons.dart';
 import 'package:christer/theme/colors.dart';
 import 'package:flutter_svg/flutter_svg.dart';
-import 'package:flutter_tindercard/flutter_tindercard.dart';
+import 'package:swipe_cards/draggable_card.dart';
+import 'package:swipe_cards/swipe_cards.dart';
+import 'package:christer/model/content.dart';
 
 class ExplorePage extends StatefulWidget {
   @override
@@ -13,155 +13,215 @@ class ExplorePage extends StatefulWidget {
 }
 
 class _ExplorePageState extends State<ExplorePage> {
-  List itemsTemp = [];
-  int itemLength = 0;
+  List<SwipeItem> _swipeItems = <SwipeItem>[];
+  MatchEngine? _matchEngine;
+  GlobalKey<ScaffoldState> _scaffoldKey = GlobalKey();
+
   @override 
   void initState(){
+    for(int i=0; i < explore_json.length; i++){
+      _swipeItems.add(SwipeItem(
+        content: Content(
+          name: explore_json[i]['name'],
+          image_path: explore_json[i]['img'],
+          age: explore_json[i]['age'],
+          interests: explore_json[i]['likes']
+        ),
+        likeAction: () {
+            print("Like action!");
+          },
+          nopeAction: () {
+            print("Nope action!");
+          },
+          superlikeAction: () {
+            print("Superlike action!");
+          },
+          onSlideUpdate: (SlideRegion? region) async {}
+        )
+      );
+    }
+
+    _matchEngine = MatchEngine(swipeItems: _swipeItems);
+
     super.initState();
-    setState(() {
-      itemsTemp = explore_json;
-      itemLength = explore_json.length;
-    });
   }
 
   @override
   Widget build(BuildContext context) {
     return Scaffold(
-      backgroundColor: white,
+      backgroundColor: black,
       body: getBody(),
-      //bottomSheet: getFooter(),
+      bottomSheet: getFooter(),
     );
   }
 
-  
   Widget getBody(){
     var size = MediaQuery.of(context).size;
     return Padding(
       padding: const EdgeInsets.only(bottom: 1),
       child: Container(
-        height: size.height,
-        child: TinderSwapCard(
-          totalNum: itemLength,
-          maxWidth: size.width,
-          maxHeight: size.height * 0.99,
-          minWidth: size.width * 0.75,
-          minHeight: size.height * 0.8,
-          cardBuilder: (context, index) => Container(
-            decoration: BoxDecoration(
-              borderRadius: BorderRadius.circular(10),
-              boxShadow: [BoxShadow(
-                color: grey.withOpacity(0.3),
-                blurRadius: 5,
-                spreadRadius: 2,
-              )]
-            ),
-            child: ClipRRect(
-              borderRadius: BorderRadius.circular(10),
-              child: Stack(
-                children: [
-                  Container(
-                    width: size.width,
-                    height: size.height,
-                    decoration: BoxDecoration(
-                      image: DecorationImage(image: AssetImage(explore_json[index]['img']), fit: BoxFit.cover),
-                    ),
-                  ),
-                  Container(
-                    width: size.width,
-                    height: size.height,
-                    decoration: BoxDecoration(
-                      gradient: LinearGradient(colors: [
-                          black.withOpacity(0.25),
-                          black.withOpacity(0),
-                        ],
-                        end: Alignment.topCenter,
-                        begin: Alignment.bottomCenter,
-
+        height: size.height * 0.75,
+        child: SwipeCards(
+          matchEngine: _matchEngine!,
+          itemBuilder: (BuildContext context, int index) {
+            return Padding(
+              padding: const EdgeInsets.all(10),
+              child: Container(
+                alignment: Alignment.center,
+                decoration: BoxDecoration(
+                  borderRadius: BorderRadius.circular(10),
+                  boxShadow: [BoxShadow(
+                    color: grey.withOpacity(0.3),
+                    blurRadius: 5,
+                    spreadRadius: 2,
+                  )]
+                ),
+                child: ClipRRect(
+                  borderRadius: BorderRadius.circular(10),
+                  child: Stack(
+                    children: [
+                      Container(
+                        width: size.width,
+                        height: size.height,
+                        decoration: BoxDecoration(
+                          image: DecorationImage(image: AssetImage(_swipeItems[index].content.image_path), fit: BoxFit.cover),
+                        ),  
                       ),
-                    ),
-                    child: Column(
-                      crossAxisAlignment: CrossAxisAlignment.start,
-                      mainAxisAlignment: MainAxisAlignment.end,
-                      children: [
-                        Padding(padding: const EdgeInsets.only(left: 15, bottom: 15),
-                          child: Row(children: [
-                            Container(
-                              width: size.width * 0.72,
-                              child: Column(
+                      Container(
+                        decoration: BoxDecoration(
+                          gradient: LinearGradient(colors: [
+                              black.withOpacity(0.5),
+                              black.withOpacity(0),
+                            ],
+                            end: Alignment.topCenter,
+                            begin: Alignment.bottomCenter,
+
+                          ),
+                        ),
+                        child: Padding(
+                          padding: const EdgeInsets.all(20),
+                          child: Column(
+                            mainAxisAlignment: MainAxisAlignment.end,
+                            children: [
+                              Row(
                                 children: [
-                                  Row(
-                                    children: [
-                                      Text(itemsTemp[index]['name'], style: TextStyle(
-                                          fontSize: 24,
-                                          color: white,
-                                          fontWeight: FontWeight.bold,
-                                        ),
-                                      ),
-                                      SizedBox(width: 10, ),
-                                      Text(itemsTemp[index]['age'], style: TextStyle(
-                                          fontSize: 22,
-                                          color: white,
-                                        ),
-                                      ),
-                                    ],
+                                  Text(_swipeItems[index].content.name + ",", style: const TextStyle(
+                                      fontSize: 24,
+                                      color: white,
+                                      fontWeight: FontWeight.bold,
+                                    ),
+                                  ),
+                                  const SizedBox(width: 10, ),
+                                  Text(_swipeItems[index].content.age, style: const TextStyle(
+                                      fontSize: 22,
+                                      color: white,
+                                    ),
                                   ),
                                 ],
                               ),
-                            ),
-                          ],)
+                            ],
+                          ),
                         ),
-                        
-                      ],
-                    ),
+                      ),
+                    ]
                   ),
-                ],
-              ),
-            ),
-          ),
+                ),
+              )
+            );
+          },
+          
+          onStackFinished: () { // DEBUG - LOOP
+            for(int i=0; i < explore_json.length; i++){
+              _swipeItems.add(SwipeItem(
+                content: Content(
+                  name: explore_json[i]['name'],
+                  image_path: explore_json[i]['img'],
+                  age: explore_json[i]['age'],
+                  interests: explore_json[i]['likes']
+                ),
+                likeAction: () {
+                  // DEBUG
+                  print("Like action!");
+                },
+                nopeAction: () {
+                  // DEBUG
+                  print("Nope action!");
+                },
+                superlikeAction: () {
+                  // DEBUG
+                  print("Superlike action!");
+                },
+                onSlideUpdate: (SlideRegion? region) async {
+                }
+                )
+              );
+            }
+          },
+
+          itemChanged: (SwipeItem item, int index) {
+                print("index: $index");
+          },
+          upSwipeAllowed: true,
+          fillSpace: true,
         ),
+        
       ),
     );
   }
-  
-
   
   Widget getFooter() {
     var size = MediaQuery.of(context).size;
     return Container(
       width: size.width,
-      height: 120,
+      height: size.height * 0.2 - 15,
       decoration: BoxDecoration(
-        color: white
+        color: black
       ),
       child: Padding(
-        padding: const EdgeInsets.only(left: 20, right: 20, bottom: 20),
+        padding: const EdgeInsets.all(20),
         child: Row(
-          mainAxisAlignment: MainAxisAlignment.spaceBetween,
+          mainAxisAlignment: MainAxisAlignment.spaceEvenly,
           children: List.generate(item_icons.length, (index) {
-            return Container(
-              width: item_icons[index]['size'],
-              height: item_icons[index]['size'],
-              decoration: BoxDecoration(
-                shape: BoxShape.circle,
-                color: white,
-                boxShadow: [
-                  BoxShadow(
-                    color: grey.withOpacity(0.1),
-                    spreadRadius: 5,
-                    blurRadius: 10,
-                  ),
-                ],
+            return ElevatedButton(
+              style: ElevatedButton.styleFrom(
+                shape: CircleBorder(),
+                padding: EdgeInsets.all(12),
               ),
-              child: Center(
-                child: SvgPicture.asset(item_icons[index]['icon'],
-                  width: item_icons[index]['icon_size']
+              child: Container(
+                width: item_icons[index]['size'],
+                height: item_icons[index]['size'],
+                decoration: BoxDecoration(
+                  shape: BoxShape.circle,
+                  color: white,
+                  boxShadow: [
+                    BoxShadow(
+                      color: grey.withOpacity(0.1),
+                      spreadRadius: 5,
+                      blurRadius: 10,
+                    ),
+                  ],
+                ),
+                child: Center(
+                  child: SvgPicture.asset(item_icons[index]['icon'],
+                    width: item_icons[index]['icon_size']
+                  ),
                 ),
               ),
+              onPressed: (){
+                if(index == 0){
+                  _matchEngine!.currentItem?.nope();
+                }
+                else if(index == 1){
+                  _matchEngine!.currentItem?.superLike();
+                } 
+                else{ // index == 2
+                  _matchEngine!.currentItem?.like();
+                }
+              },
             );
           })
         ),
       ),
     );
   }
-  
 }
